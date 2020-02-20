73fb85fba8b1fe150c47b1a18510821012f16c1f
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-02-20 16:15:40 +0000

    Notification Lab creates notifications

--- NotificationLab/app/src/main/java/com/example/notificationlab/MainActivity.java
+++ NotificationLab/app/src/main/java/com/example/notificationlab/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.notificationlab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button myButton;
+    private int notificationID=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton=findViewById(R.id.button);
+
+        createNotificationChannel();
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder builder=buildNotification(1,pendingIntent);
+                // notificationId is a unique int for each notification that you must define
+                notificationManager.notify(notificationID, builder.build());
+                notificationID++;
+            }
+        });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("Ross Channel", "Notifcation From Ross", importance);
+            channel.setDescription("Heres a notifcation!!!");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private NotificationCompat.Builder buildNotification(int delay, PendingIntent pendingIntent){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Ross Channel")
+                .setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
+                .setContentTitle("Notification Lab")
+                .setContentText("Here is your notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+                return builder;
     }
 }
